d33b4e8a248b06b3c0d669b481e89ba25aa12942
RealsSec <ovhunhittablelol@courvix.com>
2023-10-05 22:27:44 -0400

    aaa

---
+++ cashapp/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    
+    document.addEventListener('contextmenu', function (event) {
+      event.preventDefault();
+    });
+  
+    
+    document.addEventListener('mousedown', function (event) {
+      if (event.button === 2) {
+        event.preventDefault();
+      }
+    });
+  });
+  
+
+
+
